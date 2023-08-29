ee6c535b5457cf12b592914c842b530f96d38142
BARAKAT <ammrbr@hotmail.com>
2023-08-29 15:17:34 +0200

    #3 - Update ResourceType Format
    - Modify the resourceType field to follow the new formatting guidelines.

--- src/main/java/org/ejprarediseases/vpdpbackend/resource/v1/model/ResourceType.java
+++ src/main/java/org/ejprarediseases/vpdpbackend/resource/v1/model/ResourceType.java
@@ -1,16 +1,10 @@
 package org.ejprarediseases.vpdpbackend.resource.v1.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public enum ResourceType {
 
-    @JsonProperty("patientRegistry")
     PATIENT_REGISTRY,
-    @JsonProperty("bioBank")
     BIO_BANK,
-    @JsonProperty("knowledgeBase")
     KNOWLEDGE_BASE,
-    @JsonProperty("catalog")
     CATALOG;
 
 }
