19d39a1b0e48e54a993867d4ff8415d513352eab
Larie Borelle Siabou <larie.borelle.siabou@ges.thm.de>
2024-02-13 09:34:15 +0100

    #35-Search-results-when-searching-for-ORPHA:635-do-not-stop
    Resolve the search problem

--- src/main/java/org/ejprarediseases/vpdpbackend/search/v1/model/beacon/response_body/result_set/individuals/IndividualsResultSetInfo.java
+++ src/main/java/org/ejprarediseases/vpdpbackend/search/v1/model/beacon/response_body/result_set/individuals/IndividualsResultSetInfo.java
@@ -2,9 +2,8 @@ package org.ejprarediseases.vpdpbackend.search.v1.model.beacon.response_body.res
 
 import lombok.Data;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@Data
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Data
 public class IndividualsResultSetInfo {
     private String contactPoint;
     private String contactEmail;
