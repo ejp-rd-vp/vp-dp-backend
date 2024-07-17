3cc6736d13e0f479550c2da56c0e4b3eda961cb4
Abishaa Vengadeswaran <38211976+vabishaa@users.noreply.github.com>
2024-07-17 13:31:33 +0200

    #61 - Adjust beacon query format of monitoring service - add prefix for filters

--- src/main/java/org/ejprarediseases/vpdpbackend/search/v1/model/beacon/enums/BeaconFilterType.java
+++ src/main/java/org/ejprarediseases/vpdpbackend/search/v1/model/beacon/enums/BeaconFilterType.java
@@ -7,19 +7,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum BeaconFilterType {
-    SEX("NCIT_C28421"),
-    DISEASE("NCIT_C2991"),
-    PHENOTYPE("SIO_010056"),
-    CAUSATIVE_GENES("data_2295"),
-    AGE_THIS_YEAR("NCIT_C83164"),
-    SYMPTOM_ONSET("NCIT_C124353"),
+    SEX("obo:NCIT_C28421"),
+    DISEASE("obo:NCIT_C2991"),
+    PHENOTYPE("sio:SIO_010056"),
+    CAUSATIVE_GENES("edam:data_2295"),
+    AGE_THIS_YEAR("obo:NCIT_C83164"),
+    SYMPTOM_ONSET("obo:NCIT_C124353"),
     ID("id"),
     NAME("name"),
     DESCRIPTION("description"),
     ORGANISATION("organisation"),
     RESOURCE_TYPES("resourceTypes"),
     COUNTRY("country"),
-    AGE_AT_DIAGNOSIS("NCIT_C156420");
+    AGE_AT_DIAGNOSIS("obo:NCIT_C156420");
 
     private final String value;
 
